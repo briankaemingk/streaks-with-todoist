f771e8f336ce9f60b7716391ff018b6a128efeac
Brian Kaemingk <brian.e.k@gmail.com>
2018-09-06 12:05:41 -0700

    Testing error catching

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -6,7 +7,10 @@ app = Flask(__name__)
 @app.route('/callback')
 def callback():
     code = request.args.get('code')
-    print(code)
+    state = request.args.get('state')
+    error = request.args.get('error')
+    print(code + ' ' + state)
+    print(error)
     #content = request.get_json()
     #print(content)
     return 'Complete'
