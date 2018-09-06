0f6a13879a089720e85d9002ee9eaae55a802da9
Brian Kaemingk <brian.e.k@gmail.com>
2018-09-06 13:06:26 -0700

    Testing error catching with logging

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-import logging
 from flask import Flask, request
+import logging
 
 app = Flask(__name__)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 @app.route('/callback')
@@ -9,8 +12,8 @@ def callback():
     code = request.args.get('code')
     state = request.args.get('state')
     error = request.args.get('error')
-    print(code + ' ' + state)
-    print(error)
+    logging.warning(code + ' ' + state)
+    logging.warning(error)
     #content = request.get_json()
     #print(content)
     return 'Complete'
