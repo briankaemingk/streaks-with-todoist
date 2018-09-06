1f97a81b1334bda09696541045299d31bb50b55f
Brian Kaemingk <brian.e.k@gmail.com>
2018-09-06 13:51:51 -0700

    Testing error catching with logging or state

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ def callback():
     code = request.args.get('code')
     state = request.args.get('state')
     if state != os.getenv('STATE') or request.args.get('error'):
-        logging.warning('Request for Todoist-Morph not authorized, exiting')
-        exit()
+        return 'Request for Todoist-Morph not authorized, exiting'
     #content = request.get_json()
     print(state)
     return 'Complete'
