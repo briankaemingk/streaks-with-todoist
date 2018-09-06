eb8aaf4fff19fce763fd2c93980036285fe7be15
Brian Kaemingk <brian.e.k@gmail.com>
2018-09-05 21:22:42 -0700

    Working version of url oauth code parse

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/callback')
+def callback():
+    code = request.args.get('code')
+    print(code)
+    #content = request.get_json()
+    #print(content)
+    return 'Complete'
 
 
 if __name__ == '__main__':
