4960d2b1d370162681a433658059ea67342d072c
Brian Kaemingk <brian.e.k@gmail.com>
2018-09-06 14:24:52 -0700

    Testing token exchange

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import logging, os
+import logging, os, requests
 
 app = Flask(__name__)
 
@@ -13,8 +13,17 @@ def callback():
     state = request.args.get('state')
     if state != os.getenv('STATE') or request.args.get('error'):
         return 'Request for Todoist-Morph not authorized, exiting. Go <a href=' + os.getenv('APP_URL') + '>back</a>'
+
+    data = {'client_id' : os.getenv('CLIENT_ID'), 'client_secret' : os.getenv('CLIENT_SECRET'), 'code' : code}
+    # sending post request and saving response as response object
+    r = requests.post(url=os.getenv('API_TOKEN_EX'), data=data)
+    print(r)
+
+    # extracting response text
+    pastebin_url = r.text
+    print("The pastebin URL is:%s" % pastebin_url)
+
     #content = request.get_json()
-    print(state)
     return 'Complete'
 
 
