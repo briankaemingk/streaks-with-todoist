410419ade9f4d51a242feebaaac60e35e032cf99
Brian Kaemingk <brian.e.k@gmail.com>
2018-09-19 11:41:18 -0700

    Fixing reminder fired webhook capture

--- reminder_fired.py
+++ reminder_fired.py
@@ -2,12 +2,10 @@ import app
 
 def main(api, task_id):
     task = api.items.get_by_id(task_id)
-    print('Reminder fired for ', task)
     due_date_utc = task["due_date_utc"]
     due_date = app.convert_time_str_datetime(due_date_utc, app.get_user_timezone(api))
     now = app.get_now_user_timezone(api)
 
     # If the task is due today and it is due in the past
     if due_date <= now and due_date.date() == now.date():
-        print('Update to all day')
-        task.update(due_date_utc=app.update_to_all_day(now))
+        task.update(due_date_utc=app.convert_datetime_str(app.update_to_all_day(now)))
