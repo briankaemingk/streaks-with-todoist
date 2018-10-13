924bf488d5a2903e8bf70611b2e3bff6c913b3e2
Brian Kaemingk <brian.e.k@gmail.com>
2018-10-13 14:56:50 -0700

    Debugging manage db

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-import settings
+import settings, os, config
 from app import app, db
 
 
-#app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 migrate = Migrate(app, db)
 manager = Manager(app)
