7c71db78803ee8ff076024e1a51379b32f46e77b
Brian Kaemingk <brian.e.k@gmail.com>
2018-11-30 15:55:32 -0800

    Removing debugging statements

--- reminder_fired.py
+++ reminder_fired.py
@@ -1,15 +1,9 @@
 import app
 
 def main(api, task_id):
-    now_date_user_time = app.get_now_user_timezone(api)
-    print('Reminder - Now user time: ', str(now_date_user_time))
     task = api.items.get_by_id(task_id)
-    print('Reminder - Task: ', task)
     now_date = app.datetime.utcnow()
-    print('Reminder - Now date utc: ', now_date)
-    now_date_user_time = app.get_now_user_timezone(api)
     now_date_all_day = app.update_to_all_day(now_date)
-    print('Reminder - Now date all day: ', now_date_all_day)
     now_string_all_day = app.convert_datetime_str(now_date_all_day)
     print('Reminder - updating task from ', task['due_date_utc'], ' to ', now_string_all_day)
     task.update(due_date_utc=now_string_all_day)
