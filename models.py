c59a4ed6ef7ebdc2311490c0baeda64515d9e84c
Brian Kaemingk <brian.e.k@gmail.com>
2018-11-29 16:23:38 -0800

    Adding feature db columns

--- models.py
+++ models.py
@@ -5,10 +5,16 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     access_token = db.Column(db.String())
+    jit_feature = db.Column(db.Boolean())
+    recurrence_resch_feature = db.Column(db.Boolean())
+    streaks_feature = db.Column(db.Boolean())
 
     def __init__(self, id, access_token):
         self.id = id
         self.access_token = access_token
+        self.jit_feature = True
+        self.recurrence_resch_feature = True
+        self.streaks_feature = True
 
     def __repr__(self):
         return '<id {}, access token {}>'.format(self.id, self.access_token)
