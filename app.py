41ea697394303febba9281596b32ced1e85b93f6
Brian Kaemingk <brian.e.k@gmail.com>
2018-12-02 09:39:10 -0800

    Avoiding duplicates: if webhook callback content matches api content

--- app.py
+++ app.py
@@ -74,11 +74,13 @@ def webhook_callback():
                 if api.items.get_by_id(item_id) is not None:
                     item = api.items.get_by_id(item_id)
                     content = item['content']
-                    print(local_time + ': Task complete: ' + content)
-                    task_complete.main(api, item_id)
-                    item = api.items.get_by_id(item_id)
-                    content = item['content']
-                    print(local_time + ': After task complete: ' + content)
+                    # Avoiding duplicates: if webhook callback content matches api content
+                    if request.json['event_data']['content'] == content:
+                        print(local_time + ': Task complete: ' + content)
+                        task_complete.main(api, item_id)
+                        item = api.items.get_by_id(item_id)
+                        content = item['content']
+                        print(local_time + ': After task complete: ' + content)
             if request.json['event_name'] == 'reminder:fired':
                 item_id = int(request.json['event_data']['item_id'])
                 if api.items.get_by_id(item_id) is not None:
