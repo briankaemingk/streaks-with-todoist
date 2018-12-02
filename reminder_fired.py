f823a653420f186204b2d6b4c9950e7956d032b7
Brian Kaemingk <brian.e.k@gmail.com>
2018-12-02 10:06:54 -0800

    Correcting next day reminder update

--- reminder_fired.py
+++ reminder_fired.py
@@ -2,7 +2,7 @@ import app
 
 def main(api, task_id):
     task = api.items.get_by_id(task_id)
-    now_date = app.datetime.utcnow()
+    now_date = app.get_now_user_timezone(api)
     now_date_all_day = app.update_to_all_day(now_date)
     now_string_all_day = app.convert_datetime_str(now_date_all_day)
     print('Reminder - updating task from ', task['due_date_utc'], ' to ', now_string_all_day)
