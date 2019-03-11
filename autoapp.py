841b7f12faff575f0e2493098f87336edb904ee9
brian kaemingk <brian.e.k@gmail.com>
2019-03-11 21:47:04 +0100

    Testing cron job fix

--- autoapp.py
+++ autoapp.py
@@ -1,5 +1,15 @@
 """Create an application instance."""
 from app.app import create_app
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 app = create_app()
 app.app_context().push()
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('cron', minute=49)
+def scheduled_job():
+    print("Timer called")
+
+
+sched.start()
