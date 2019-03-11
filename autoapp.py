793059f24ea1f8583113c269c921b5469656dffc
brian kaemingk <brian.e.k@gmail.com>
2019-03-11 22:11:38 +0100

    Testing cron job fix

--- autoapp.py
+++ autoapp.py
@@ -1,18 +1,5 @@
 """Create an application instance."""
 from app.app import create_app
-from apscheduler.schedulers.background import BackgroundScheduler
-import atexit
-
-def print_date_time():
-    print('Timer run')
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(func=print_date_time, trigger="interval", seconds=3)
-scheduler.start()
-
-# Shut down the scheduler when exiting the app
-atexit.register(lambda: scheduler.shutdown())
-
 
 app = create_app()
 app.app_context().push()
